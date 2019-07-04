5916f95f0e3bda844a729c099a33ca4177d8e59b
Joel Ibaceta <mail@joelibaceta.com>
2019-07-04 13:02:49 -0500

    no meetup uris support

--- _plugins/meetup_members_counter_tag.rb
+++ _plugins/meetup_members_counter_tag.rb
@@ -10,25 +10,33 @@ module Jekyll
             @meetup_uri = text
         end
 
-        def get_meetup_data(uri)
-            uri_pattern = /.+\/(?<uri_name>[\w\-]+).*/
+        def is_meetup_uri?(uri)
+            uri_pattern = /meetup.com/
             match = uri_pattern.match(uri)
-            if !(match)
-                raise 'malformed meetup uri: ' + uri
+            return match != nil
+        end
+
+        def get_meetup_data(uri)
+            if is_meetup_uri?(uri)
+                uri_pattern = /.+\/(?<uri_name>[\w\-]+).*/
+                match = uri_pattern.match(uri)
+                if !(match)
+                    raise 'malformed meetup uri: ' + uri
+                end
+                request_uri  = "https://api.meetup.com/#{match[:uri_name]}"
+                encoded_uri = URI.encode(request_uri)
+                meetup_uri = URI.parse(encoded_uri)
+                puts "Getting members count from #{uri}"
+                response = Net::HTTP.get_response(meetup_uri)
+                meetup_data = JSON.parse(response.body)
+                return meetup_data
             end
-            request_uri  = "https://api.meetup.com/#{match[:uri_name]}"
-            encoded_uri = URI.encode(request_uri)
-            meetup_uri = URI.parse(encoded_uri)
-            puts "Getting members count from #{uri}"
-            response = Net::HTTP.get_response(meetup_uri)
-            meetup_data = JSON.parse(response.body)
-            return meetup_data
         end
 
         def render(context)
             uri = context[@meetup_uri] || @meetup_uri
             meetup_data = get_meetup_data(uri)
-            return "#{meetup_data["members"]}"
+            return meetup_data ? "<span>#{meetup_data["members"]}</span> miembros" : nil
         end
     end
 end
