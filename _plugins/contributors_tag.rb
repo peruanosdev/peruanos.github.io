4f4ce0889e1daf7ac2a5a11cc7f2636357ae2faa
Joel Ibaceta <mail@joelibaceta.com>
2019-10-08 22:12:14 -0500

    add contributors section

---
+++ _plugins/contributors_tag.rb
@@ -0,0 +1,37 @@
+require 'net/http'
+require 'uri'
+
+module Jekyll
+    class ContributorsTag < Liquid::Tag
+
+        def initialize(tag_name, text, tokens)
+            super
+        end
+        
+        def getContributors
+            uri = URI.parse("https://api.github.com/repos/peruanosdev/peruanos.github.io/contributors")
+            response = Net::HTTP.get_response(uri)
+
+            contributors = JSON.parse(response.body)
+            return contributors
+        end
+
+        def render(context)
+            contributors = getContributors
+
+            element = "<div class='contributors'>"
+            contributors.each do |contributor|
+                p contributor
+                element += "<div class='contributor'>"
+                element += "    <a href='#{contributor["html_url"]}'>"
+                element += "        <img class='User__image' src='#{contributor["avatar_url"]}'>"
+                element += "    </a>"
+                element += "</div>"
+            end
+            element += "</div>"
+            return element
+        end
+    end
+end
+
+Liquid::Template.register_tag('contributors', Jekyll::ContributorsTag)
